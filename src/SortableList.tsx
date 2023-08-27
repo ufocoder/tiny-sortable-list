252fe096a7dba880476295e1b25c87adea97f618
Sergey Ufocoder <xufocoder@gmail.com>
2023-08-27 17:50:08 +0300

    chore: add draggable item

--- src/SortableList.tsx
+++ src/SortableList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, SyntheticEvent, cloneElement } from 'react'
+import React, { useState, useEffect, useCallback, SyntheticEvent, cloneElement, Children, useRef } from 'react'
 
 enum Position {
   before = 0,
@@ -9,10 +9,10 @@ export type Direction = 'vertical' | 'horizontal'
 
 export interface SortableItemProps<T> {
   item: T
-  isDragItemInsertBefore: boolean
-  isDragItemInsertAfter: boolean
-  isDragged: boolean
-  isHovered: boolean
+  isDragItemInsertBefore?: boolean
+  isDragItemInsertAfter?: boolean
+  isDragged?: boolean
+  isHovered?: boolean
 }
 
 export interface SortableListProps<T> {
@@ -91,10 +91,47 @@ function calculationTargetIndex(position: Position, sourceIndex: number, index: 
     return index + 1
 }
 
+
+interface DraggableItemProps<T> {
+  item: T;
+  itemRect: DOMRect;
+  index: number;
+  coords: Coords;
+  children: (props: SortableItemProps<T>, index: number) => React.ReactElement;
+}
+
+function DraggableItem<T>(props: DraggableItemProps<T>) {
+  const { item, itemRect, index, coords,  } = props;
+
+  return (
+    <div style={{
+      position: 'absolute',
+      zIndex: 9,
+      left: coords.x,
+      top: coords.y,
+      width: itemRect.width,
+      height: itemRect.height,
+      pointerEvents: 'none'
+    }}>
+      {props.children({ item }, index)}
+    </div>
+  )
+}
+
+
+interface Coords {
+  x: number;
+  y: number;
+}
+
 export function SortableList<T>(props: SortableListProps<T>) {
     const [sourceIndex, setSourceIndex] = useState<number | null>(null)
     const [hoveredIndex, setHoveredIndex] = useState<number| null>(null)
     const [targetIndex, setTargetIndex] = useState<number | null>(null)
+
+    const [sourceRect, setSourceRect] = useState<DOMRect>()
+    const [pointerDownPosition, setPointerDownPosition] = useState<Coords>({ x: 0, y: 0 });
+    const [pointerMovePosition, setPointerMovePosition] = useState<Coords>({ x: 0, y: 0 });
 
     const { items, setItems, direction = 'vertical', className, style } = props
 
@@ -118,7 +155,7 @@ export function SortableList<T>(props: SortableListProps<T>) {
 
 
     useEffect(() => {
-      const handlePointerUp = (e: any) => {
+      const handlePointerUp = (e: PointerEvent) => {
         e.preventDefault()
 
         if (sourceIndex !== null && targetIndex !== null) {
@@ -138,17 +175,61 @@ export function SortableList<T>(props: SortableListProps<T>) {
     })
 
 
+    useEffect(() => {
+      const handlePointerMove = (e: PointerEvent) => {
+        e.preventDefault()
+
+        if (sourceIndex == null) {
+          return
+        }
+
+        setPointerMovePosition({ x: e.pageX, y: e.pageY })
+      }
+
+      document.addEventListener('pointermove', handlePointerMove);
+
+      return () => {
+        document.removeEventListener('pointermove', handlePointerMove);
+      };
+    })
+
     return (
         <div className={className} style={style}>
+          {sourceIndex !== null && items[sourceIndex] 
+            ? <DraggableItem 
+                item={items[sourceIndex]}
+                itemRect={sourceRect!}
+                index={sourceIndex} 
+                coords={{
+                  x: pointerMovePosition.x - pointerDownPosition.x,
+                  y: pointerMovePosition.y - pointerDownPosition.y,
+                }}
+                children={props.children} /> 
+            : null}
           {items.map((item, index) => {
+            const itemRef = useRef<HTMLDivElement>(null);
 
-            const handlePointerDown = () => setSourceIndex(index)
+            const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+              if (!itemRef.current) {
+                return
+              }
+
+              setSourceRect(e.currentTarget.getBoundingClientRect())
+              setPointerDownPosition({ x: e.pageX, y: e.pageY })
+              setSourceIndex(index)
+
+              return;
+            };
+
             const handlePointerEnter = () => setHoveredIndex(index)
 
             const handlePointerOver = (e: React.PointerEvent<HTMLDivElement>) => {
               e.preventDefault()
 
+              
+
               if (sourceIndex === null) {
+                console.log('hover', item)
                 setHoveredIndex(index);
                 return
               }
@@ -158,9 +239,10 @@ export function SortableList<T>(props: SortableListProps<T>) {
 
               setTargetIndex(targetIndex)
             }
-            
+
             return (
               <div
+                ref={itemRef}
                 key={index}
                 onPointerDown={handlePointerDown}
                 onPointerEnter={handlePointerEnter}
