d8bf8ca5572f03406e7418332172c53136cfa213
Sergey Ufocoder <xufocoder@gmail.com>
2023-09-24 19:04:08 +0300

    feat: rethink eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import path from 'path';
-import dts from "vite-plugin-dts";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import eslint from "vite-plugin-eslint";
+import react from "@vitejs/plugin-react";
 import packageJson from './package.json';
 import pkg from "./package.json" assert { type: "json" };
 
@@ -12,6 +13,13 @@ export default defineConfig({
     }),
     dts({
       insertTypesEntry: true,
+      exclude: 'src/**/*.stories.tsx',
+    }),
+    eslint({
+      exclude: [
+        /virtual:/, 
+        /node_modules/
+      ],
     }),
   ],
   server: {
